424ddc2fb2635d9712b576a0783e7c615980771e
Serkan Konukcu <serkankonukcu7@outlook.com>
2024-08-07 19:34:39 +0300

    added new music

--- music.js
+++ music.js
@@ -12,10 +12,11 @@ class Music {
 }
 
 const musicList = [
-    new Music("Nocturne", "Bor Pro", "1.jpeg", "1.mp3"),    
+    new Music("Kufi", "Duman", "1.jpeg", "1.mp3"),    
     new Music("Bu da Geçer mi Sevgilim", "Yalın","2.jpeg","2.mp3"),    
     new Music("Aramızda Uçurumlar", "Suat Suna","3.jpeg","3.mp3"),
     new Music("Move On", "Deeprise", "4.jpeg", "4.mp3"),
     new Music("Küle Dönmüşsün", "Semicenk", "5.jpeg", "5.mp3"),
-    new Music("Tereddüt", "Kayra", "6.jpeg", "6.mp3")
+    new Music("Tereddüt", "Kayra", "6.jpeg", "6.mp3"),
+    new Music("Nocturne", "Bor Pro", "7.jpeg", "7.mp3")
 ];
